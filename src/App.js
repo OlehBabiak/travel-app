bd81957d44896617c7f3101907b40aae11ed66e4
Oleh <oleg_bob@ukr.net>
2022-06-11 13:00:26 +0300

    signIn, signUp pages created, footer nailed to the bottom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React, {Fragment} from "react";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./components/Home/HomePage";
-import SignUp from "./components/SingUp/SignUp";
-import classes from './index.css'
+import SignUp from "./components/SingUp/SignUpPage";
+import SignInPage from "./components/SignIn/SignInPage";
 
 function App () {
     return (
-        <div className={classes['wrapper']}>
+        <Fragment>
             <Header/>
             {/*<HomePage/>*/}
-            <SignUp/>
+            {/*<SignUp/>*/}
+            <SignInPage/>
             <Footer/>
-        </div>
+        </Fragment>
     );
 }
 
--- src/components/SignIn/SignInPage.js
+++ src/components/SignIn/SignInPage.js
@@ -1,7 +1,35 @@
+import classes from './SignIn.module.css'
+import Input from "../UI/Input";
+import Button from "../UI/Button";
+
 
 const SignInPage = () => {
     return (
-       <div>SignInPage</div>
+        <main className={classes['sign-in-page']}>
+            <form className={classes['sign-in-form']} autoComplete="off">
+                <h2 className={classes['sign-in-form__title']}>Sign In</h2>
+                <Input
+                    name="email"
+                    type="email"
+                    label='Email'
+                />
+                <Input
+                    name="password"
+                    type="password"
+                    label='Password'
+                />
+                <Button
+                    type="submit"
+                    className={classes['button']}
+                >
+                    Sign In
+                </Button>
+            </form>
+            <span>
+        Already have an account?
+        <a href="/" className={classes['sign-in-form__link']}>Sign Up</a>
+      </span>
+        </main>
     );
 }
 
